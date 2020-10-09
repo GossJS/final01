18d1ae4982e389cd188577d33d52c08128b3407c
Ilya Goss <goss@corp.ifmo.ru>
2020-10-09 05:39:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 require('http')
 .Server((req, res) => {
-  res.end(process.versions.v8);
+  if (req.url === '/v8') return res.end(process.versions.v8); // 6.8.275.32-node.55
+  if (req.url === '/day') return res.end(String(new Date(). getDate())); // текущий день от 0 до 31
+  if (req.url.startsWith('/mirror?x=')) {
+    res.end(req.url.split('x=')[1]); 
+  }
+  res.end('Ilya Gosudarev');
 })
 .listen(process.env.PORT);
